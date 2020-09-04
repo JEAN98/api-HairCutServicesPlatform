eafd9fb8f0138a6e78a25962466a7bc67a13ec7c
Jean Carlo <jeancarlovegabejarano@hotmail.com>
2020-09-04 11:59:54 -0600

    regex time was added

--- src/middleware/validators/hairdressingSalon.validator.js
+++ src/middleware/validators/hairdressingSalon.validator.js
@@ -1,11 +1,15 @@
 const { Joi } = require('express-validation')
 
+const regexTimeValidation = new RegExp(/(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)/);
 const loginValidation = {
     body: Joi.object({
       name: Joi.string().alphanum().max(100).required(),
       description: Joi.string().alphanum().max(500).required(),
       email: Joi.string().email().required(),
-      password: Joi.string().min(6).required()
+      password: Joi.string().min(6).required(),
+      shiftStarts: Joi.string().regex(regexTimeValidation).required(),
+      shiftEnds: Joi.string().regex(regexTimeValidation).required(),
+      lunchTime: Joi.string().regex(regexTimeValidation).required()
     }),
 }
 
