de2d1bd210e5685d1982fa0bb613d0cb3054a9d1
Jean Carlo <jeancarlovegabejarano@hotmail.com>
2020-09-04 14:14:36 -0600

    new validations added successfully

--- src/middleware/validators/hairdressingSalon.validator.js
+++ src/middleware/validators/hairdressingSalon.validator.js
@@ -9,7 +9,12 @@ const loginValidation = {
       password: Joi.string().min(6).required(),
       shiftStarts: Joi.string().regex(regexTimeValidation).required(),
       shiftEnds: Joi.string().regex(regexTimeValidation).required(),
-      lunchTime: Joi.string().regex(regexTimeValidation).required()
+      lunchTime: Joi.string().regex(regexTimeValidation).required(),
+      latitud: Joi.number().strict().required(),
+      longitud: Joi.number().strict().required(),
+      photo: Joi.array().items(Joi.string()).min(1).optional(),
+      website: Joi.string().uri().optional(),
+      genderID: Joi.number().strict().integer().required()
     }),
 }
 
