053824507af8327d0a307f3389f1d54f6dbfe3bf
Jean Carlo <jeancarlovegabejarano@hotmail.com>
2020-09-07 21:41:05 -0600

    createPlatformAccount validator was added

---
+++ src/middleware/validators/haircutPlatformAccount.validator.js
@@ -0,0 +1,13 @@
+const { Joi } = require('express-validation')
+
+const createPlatformAccount = {
+    body: Joi.object({
+    firstName: Joi.string().max(100).required(),
+    lastName: Joi.string().max(100).required(),
+    birthday: Joi.date().optional(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(6).required(),  
+    }),
+}
+
+module.exports = createPlatformAccount;
