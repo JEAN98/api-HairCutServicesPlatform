815316f3997f09e46bc914fc68bd784d138c19ae
Jean Carlo <jeancarlovegabejarano@hotmail.com>
2020-09-16 23:14:30 -0600

    workerResource needs a certain level of permissions

--- src/controllers/worker.controller.js
+++ src/controllers/worker.controller.js
@@ -1,6 +1,7 @@
 const repository = require('../repositories/worker.repository');
-const {GeneralError,BadRequestSequelizeError,BadRequest}  = require('../middleware/error/error');
+const {GeneralError,BadRequestSequelizeError,BadRequest,Unauthorized}  = require('../middleware/error/error');
 const Sequelize = require('sequelize');
+const {checkPermissionLevel} = require('../utils/checkAccess.helper');
 
 exports.findByHairDressingSalon = async(req, res,next) => {
    try {
@@ -17,16 +18,22 @@ exports.findByHairDressingSalon = async(req, res,next) => {
 exports.create = async(req,res,next) => {
    try
    {
+      checkPermissionLevel(req.token.accountType,'HairdressingSalon');
+
       req.body.hairdressingSalonID = req.token.sub
       let data = await repository.create(req.body);
       res.status(200).send(data);
    } 
    catch (error) {
+      if(error instanceof Unauthorized)
+      {
+          next(error)
+      }
       if (error.constructor.prototype instanceof Sequelize.BaseError)
       {
          next(new BadRequestSequelizeError(error));  
       }
       else
-         next(new GeneralError("Internal server error"));  
+         next(new GeneralError("Internal server error",500));  
    }
 }
