8cf2cbe7907f68d41acaac9946c3feba16d6429f
Jean Carlo <jeancarlovegabejarano@hotmail.com>
2020-09-20 12:02:00 -0600

    gender_id was removed from body response

--- src/repositories/worker.repository.js
+++ src/repositories/worker.repository.js
@@ -1,7 +1,7 @@
 'use strict';
 const Worker = require('../config/db.config').worker;
 const cleanHelper = require('../utils/cleanEntity.helper');
-const attributesToBeRemoved = ['createdAt','updatedAt'];
+const attributesToBeRemoved = ['createdAt','updatedAt','gender_id'];
 
 exports.getWorkersByHairdressingSalon = async(query) => {
     let workerList = await Worker.findAll(
@@ -10,7 +10,7 @@ exports.getWorkersByHairdressingSalon = async(query) => {
                 hairdressing_salon_id: query.hairdressingSalonID,
                 is_active: query.isActive != undefined ? query.isActive : true
             },
-	}); //TODO: Review this json result, since gender_id and genderID are being displayed
+	}); 
     return cleanHelper.cleanEntityList(workerList,attributesToBeRemoved);
 }
 
