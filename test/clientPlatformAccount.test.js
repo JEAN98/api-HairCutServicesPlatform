7f9a88a5735715b7beb118912f52f92a8dba9783
Jean Carlo <jeancarlovegabejarano@hotmail.com>
2020-09-20 18:34:26 -0600

    haircutPlatformAccount tests were added

---
+++ test/clientPlatformAccount.test.js
@@ -0,0 +1,82 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+const expect = require('chai').expect;
+let url= 'http://localhost:3000/api/';
+let hSalonPath = 'haircutPlatformAccount';
+
+
+describe('haircutPlatformAccount suites. Post/',()=>{
+    
+    let haircutPlatformAccount = {
+        firstName: "Andrew",
+        lastName: "Vega",
+        birthday: "1998/07/22",
+        email:"salonTest@gmail.com",
+        password:"Admin@123",
+        genderID: 2
+    };
+
+    it('should get a bad request error based on email already exists', (done) => {
+        chai.request(url)
+        .post(hSalonPath)
+        .send(          
+            haircutPlatformAccount
+        )
+        .end( function(err,res){
+            expect(res).to.have.status(400);
+            expect(res.body.details.error).to.be.equals('The email already exists as HairdressingSalon');
+            done();
+          });
+    });
+
+    it('should get a bad request error based on invalid genderID', (done) => {
+        let currentPA = haircutPlatformAccount;
+        currentPA.genderID = 50478;
+        currentPA.email= 'email@qa.com';
+
+        chai.request(url)
+        .post(hSalonPath)
+        .send(          
+            currentPA
+        )
+        .end( function(err,res){
+            expect(res).to.have.status(400);
+            expect(res.body.details.message).to.be.equals('Some of the constraints are not defined properly or they does not exist in the database');
+            done();
+          });
+    });
+
+
+    it('should get a bad request error based on a missing field', (done) => {
+        chai.request(url)
+        .post(hSalonPath)
+        .send(          
+            {
+                firstName: "Mirella"
+            }
+        )
+        .end( function(err,res){
+            expect(res).to.have.status(400);
+            expect(res.body.details.name).to.be.equals('ValidationError');
+            done();
+          });
+    });
+
+    it('should get a bad request error based on an invalid field', (done) => {
+        let currentPA = haircutPlatformAccount;
+        currentPA.password = 123;
+        currentPA.lastName = 85;
+
+        chai.request(url)
+        .post(hSalonPath)
+        .send(          
+            currentPA
+        )
+        .end( function(err,res){
+            expect(res).to.have.status(400);
+            expect(res.body.details.name).to.be.equals('ValidationError');
+            done();
+          });
+    });
+
+});
