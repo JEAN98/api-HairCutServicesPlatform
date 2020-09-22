ca603bd90910a7a9ed2a7bb76c4f05c73492582a
Jean Carlo <jeancarlovegabejarano@hotmail.com>
2020-09-22 13:26:43 -0600

    percetange function was updated

--- src/services/report.service.js
+++ src/services/report.service.js
@@ -5,9 +5,16 @@ exports.getPercentageOfServicesByHSalon = async(hairdressingSalonID) => {
      
     let appoimentServicesList = await reportRepository.getAppoimentServicesListByHS(hairdressingSalonID);
     let percentageOfServicesByHSalonList = [];
+
+    let totalCount = 0;
+    appoimentServicesList.forEach(service => {
+        totalCount += parseInt(service.count);
+   });
+    
+    console.log(appoimentServicesList,totalCount)
     for (let index = 0; index < appoimentServicesList.length; index++) {
-        let service = appoimentServicesList[index].toJSON();
-        service.percentage = (service.count * 100) / appoimentServicesList.length;
+        let service = appoimentServicesList[index];
+        service.percentage = (parseInt(service.count) * 100) / totalCount;
         service.serviceID = service.service_id;
         delete  service.service_id;  
         percentageOfServicesByHSalonList.push(service);
