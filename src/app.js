b28848ffd89baee7d4bc0cb19b403fd20b655dbe
JEAN98 <jeancarlovegabejarano@hotmail.com>
2020-09-26 13:02:54 -0600

    split token was updated

--- src/app.js
+++ src/app.js
@@ -40,15 +40,12 @@ app.use(function (req,res,next){
 });
 
 // Enable CORS ()
-/*
-if(config.MODE == 'development') {
-    app.use(function (req, res, next) {
-        res.header('Access-Control-Allow-Origin', '*');
-        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, x-access-token');
-        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-        next();
-    });
-} */
+
+if(process.env.CURRENT_ENV == 'Dev') {
+  console.log('CorsEnable in Dev env')
+  var cors = require('cors')
+  app.use(cors())
+} 
 
 
 // error handler middleware
