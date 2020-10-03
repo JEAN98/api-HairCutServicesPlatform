a8d576e00a43a0b14697cc755e56de0f13df0a4a
JEAN98 <jeancarlovegabejarano@hotmail.com>
2020-10-03 11:54:29 -0600

    Facebook model was created

---
+++ src/models/facebookAccount.model.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, Sequelize) => {
+    var facebookAccount = sequelize.define('facebook_accounts', {
+    id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+    },
+    email: {
+          type: Sequelize.STRING,
+          allowNull: false    
+    },
+    facebookID: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'facebook_id',
+    },
+    clientID: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'client_id',
+    },
+    createdAt: {
+      type: Sequelize.DATE,
+      field: 'created_at',
+    },
+    updatedAt: {
+      type: Sequelize.DATE,
+      field: 'updated_at'
+    }
+  });
+  return facebookAccount;
+}
