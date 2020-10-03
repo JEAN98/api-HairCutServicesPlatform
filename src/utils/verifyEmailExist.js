3f40f08c37e99af4bb3cb09bc8bbb625f2ca9718
JEAN98 <jeancarlovegabejarano@hotmail.com>
2020-10-03 12:20:02 -0600

    unncessary funciton was removed

--- src/utils/verifyEmailExist.js
+++ src/utils/verifyEmailExist.js
@@ -13,29 +13,6 @@ exports.isEmailExist = async (newEmail) => {
             value: newEmail
         };
     }
-    /*
-    let account = await hairdressingSalonRepository.findByEmail(newEmail);
-    if(account.length > 0)
-    {
-        return buildErrorMessage("HairdressingSalon",newEmail);
-    }
-    
-    let platformClient = await haircutPlatformClient.findByEmail(newEmail);
-    if(platformClient.length > 0)
-    {
-        return buildErrorMessage("Client",newEmail);
-    }*/
-
-
     return {isEmailAccepted: true}    
-}
-
-const buildErrorMessage = (errorFrom,email) => {
-    console.log(errorFrom)
-    return {
-        error: 'The email already exists',
-        isEmailAccepted: false,
-        value: email
-    }
 }
 
