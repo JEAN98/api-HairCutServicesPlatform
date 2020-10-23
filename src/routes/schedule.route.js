539d57c65ed959b938240b58de00496491309e44
JEAN98 <jeancarlovegabejarano@hotmail.com>
2020-10-23 12:17:11 -0600

    getSchedulesBy HairdressingSalon

--- src/routes/schedule.route.js
+++ src/routes/schedule.route.js
@@ -8,4 +8,5 @@ module.exports = function(app) {
 
   app.post('/api/schedule', auth.autentication,validate(createSchedule, {}, {}),controller.create)
   app.get('/api/schedule', auth.autentication ,controller.getByHairdressingSalon);
+  app.get('/api/schedule/:id', auth.autentication ,controller.getSchedulesByHairdressingSalon);
 }
